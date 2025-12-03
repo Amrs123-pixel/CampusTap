ee162df7420f63c6dc2fd25b75e17701af7b5a36
Amrs123-pixel <simplescastillo@gmail.com>
2025-12-03 23:13:51 +0800

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,4 @@
-// firebase-config.js
+// firebase-config.js (FIXED)
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
@@ -13,12 +13,12 @@ const firebaseConfig = {
   messagingSenderId: "301788997959",
   appId: "1:301788997959:web:d3c8f1e6b2c7d32b488462",
   measurementId: "G-CE67W70TBS",
-  databaseURL: "https://campustap2-default-rtdb.asia-southeast1.firebasedatabase.app"
+  databaseURL: "https://campustap2-default-rtdb.asia-southeast1.firebasedatabase.app/"
 };
 
-const app = initializeApp(firebaseConfig);
+const app  = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getFirestore(app);
+const db   = getFirestore(app);
 const rtdb = getDatabase(app);
 
 export { app, auth, db, rtdb };
