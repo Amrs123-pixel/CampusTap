dde74782e077ca1cdc190f60f121a7924c4c8755
Amrs123-pixel <simplescastillo@gmail.com>
2025-12-04 09:50:55 +0800

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,5 @@
-// firebase-config.js (FIXED)
+// firebase-config.js
+// Paste your own config from Firebase Console → Project settings → General
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
@@ -6,19 +7,18 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.0/firebas
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAbCIJt3182dl6tPnnIt5AwOgxzB6b9efg",
-  authDomain: "campustap2.firebaseapp.com",
-  projectId: "campustap2",
-  storageBucket: "campustap2.firebasestorage.app",
-  messagingSenderId: "301788997959",
-  appId: "1:301788997959:web:d3c8f1e6b2c7d32b488462",
-  measurementId: "G-CE67W70TBS",
-  databaseURL: "https://campustap2-default-rtdb.asia-southeast1.firebasedatabase.app/"
+  apiKey: "YOUR_FIREBASE_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.asia-southeast1.firebasedatabase.app"
 };
 
-const app  = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db   = getFirestore(app);
+const db = getFirestore(app);
 const rtdb = getDatabase(app);
 
 export { app, auth, db, rtdb };
