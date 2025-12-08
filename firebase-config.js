2490fd7f6e57d0174d0234d0da205e413b82a61e
Amrs123-pixel <simplescastillo@gmail.com>
2025-12-09 03:32:44 +0800

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,29 +1,13 @@
-// firebase-config.js
-
-// Firebase Imports (Modular SDK)
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-database.js";
-
-// Your REAL Firebase Config (from screenshot)
+// Firebase config (same keys)
 const firebaseConfig = {
- apiKey: "AIzaSyA6OmfsgRx-D6dN2Mto5Btczi8A64ReQJg",
-  authDomain: "campustap-522d8.firebaseapp.com",
-  databaseURL: "https://campustap-522d8-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "campustap-522d8",
-  storageBucket: "campustap-522d8.firebasestorage.app",
-  messagingSenderId: "929159058918",
-  appId: "1:929159058918:web:2378bbb19f873e98dcb227"
+    apiKey: "AIzaSyBDQvjQ968Roz-03vYmbIP7zD6TJZuINu8",
+    authDomain: "campustap-522d8.firebaseapp.com",
+    projectId: "campustap-522d8",
+    storageBucket: "campustap-522d8.appspot.com",
+    messagingSenderId: "461548181335",
+    appId: "1:461548181335:web:c26a6e0bdb5e61d45abec5",
+    measurementId: "G-9PS2LX52RR"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-// Services
-const auth = getAuth(app);
-const db = getFirestore(app);
-const rtdb = getDatabase(app);
-
-// Export for use in all HTML pages
-export { app, auth, db, rtdb };
+// GLOBAL FIREBASE INITIALIZATION
+firebase.initializeApp(firebaseConfig);
