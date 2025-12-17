40fa5e929523b1c77b32ddc0a2904e245c7371ae
alvinjsc <iialvinjohn@gmail.com>
2025-12-17 14:52:34 +0800

    Initial commit - CampusTap with Google Maps integration

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Firebase v9 COMPAT config
+const firebaseConfig = {
+    apiKey: "AIzaSyBDQvjQ968Roz-03vYmbIP7zD6TJZuINu8",
+    authDomain: "campustap-522d8.firebaseapp.com",
+    projectId: "campustap-522d8",
+    storageBucket: "campustap-522d8.appspot.com",
+    messagingSenderId: "461548181335",
+    appId: "1:461548181335:web:c26a6e0bdb5e61d45abec5"
+};
+
+// Initialize app
+firebase.initializeApp(firebaseConfig);
+
+// Global references
+const auth = firebase.auth();
+const db   = firebase.firestore();
+const rtdb = firebase.database();
